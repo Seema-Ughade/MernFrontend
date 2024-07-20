fc040cb6f093eeb4443bf613a3803f306cf7c9ce
seema <seemarughade1363@gmail.com>
2024-07-20 21:16:45 +0530

    render link added

--- src/components/ApiFetch.jsx
+++ src/components/ApiFetch.jsx
@@ -6,7 +6,7 @@ function ApiFetch() {
 
   const getData = async () => {
   
-      const response = await axios.get('http://localhost:4000/albums');
+      const response = await axios.get('https://mern-backendnew.onrender.com/albums');
       setData(response.data);
    
   };
